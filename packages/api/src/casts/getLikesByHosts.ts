29980c274a400f55fefa2fff943d07e47c071c11
Boldizsar Mezei <boldizsar@unchained.work>
2024-06-18 15:46:07 +0200

    Leaderboard
    Fixes

--- packages/api/src/casts/getLikesByHosts.ts
+++ packages/api/src/casts/getLikesByHosts.ts
@@ -2,6 +2,7 @@ import { Dayjs } from "dayjs";
 import { database } from "../db";
 import { Cast } from "../interfaces/casts";
 import { bytesToHexString } from "@farcaster/hub-nodejs";
+import { getBannedFids } from "../users/getBanned";
 
 export const getCastsLikedByHosts = async (
   channelId: string,
@@ -9,10 +10,11 @@ export const getCastsLikedByHosts = async (
   startTime: Dayjs,
   endTime: Dayjs
 ) => {
+  const bannedFids = await getBannedFids(channelId);
   const casts = await database("casts as c")
     .select("c.*")
     .where({ parent_url: `https://warpcast.com/~/channel/${channelId}` })
-    .whereNotIn("c.fid", hosts)
+    .whereNotIn("c.fid", [...hosts, ...bannedFids])
     .whereBetween("c.timestamp", [startTime.toDate(), endTime.toDate()])
     .join("reactions as r", function () {
       this.on("c.hash", "=", "r.target_cast_hash")
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,27 +2,59 @@ import cors from "cors";
 import dayjs from "dayjs";
 import express from "express";
 import { leaderboard } from "./routes/leaderboard";
+import { getInsecureHubRpcClient } from "@farcaster/hub-nodejs";
+import { waitForReadyHubClient } from "./utils";
+import { banUserFromChannel } from "./routes/ban_user";
 
 const port = 8080;
 const app = express();
 
 app.use(cors());
 
-app.use("/*", (req, _res, next) => {
-  const baseUrl = req.baseUrl;
-  console.log(`Captured base URL: ${baseUrl}`);
+app.use("/*", (_req, _res, next) => {
   next();
 });
 
-app.use("/farcaster/leaderboard", async (req: express.Request, res: express.Response) => {
-  try {
-    const channelId = req.query.channelId as string;
-    const castedAfter = dayjs.unix(Number(req.query.castedAfter));
-    const castedBefore = dayjs.unix(Number(req.query.castedBefore));
-    res.send(await leaderboard(channelId, castedBefore, castedAfter));
-  } catch (err: any) {
-    res.status(err.status || 400).send({ error: err.error || "Unkown" });
+app.get(
+  "/farcaster/leaderboard",
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const channelId = req.query.channelId as string;
+      const castedAfter = dayjs.unix(Number(req.query.castedAfter));
+      const castedBefore = dayjs.unix(Number(req.query.castedBefore));
+      res.send(await leaderboard(channelId, castedBefore, castedAfter));
+    } catch (err: any) {
+      console.log(err);
+      res.status(err.status || 400).send({ error: err.error || "Unkown" });
+    }
   }
+);
+
+const hub = getInsecureHubRpcClient(process.env.HUB_HOST!);
+
+app.post(
+  "/farcaster/ban",
+  async (req: express.Request, res: express.Response) => {
+    await waitForReadyHubClient(hub);
+    try {
+      res.send(await banUserFromChannel(hub, req.body));
+    } catch (err: any) {
+      res.status(err.status || 400).send({ error: err.error || "Unkown" });
+    }
+  }
+);
+
+app.get("/farcaster/ban", (req, res) => {
+  res.send({
+    aboutUrl: "https://api.buildcore.io/farcaster/ban",
+    action: {
+      type: "post",
+    },
+    name: "Ban User! /justbuild",
+    description: "Custom Action to ban users from the justbuild.",
+    icon: "mute",
+    postUrl: "https://api.buildcore.io/farcaster/ban",
+  });
 });
 
 const server = app.listen(port);
